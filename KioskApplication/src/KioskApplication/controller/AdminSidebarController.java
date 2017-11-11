4d4b09b496cb2304f8407ace7e117a4f3b2a4765
dxu2 <xuda980520@gmail.com>
2017-11-11 17:00:22 -0500

    Implement add node screen.

--- KioskApplication/src/KioskApplication/controller/AdminSidebarController.java
+++ KioskApplication/src/KioskApplication/controller/AdminSidebarController.java
@@ -1,11 +1,28 @@
 package KioskApplication.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static KioskApplication.controller.AdminWindowController.SidebarType.SIDEBAR_ADD;
 
 public class AdminSidebarController {
+
+    AdminWindowController parent;
+
+    AdminSidebarController(AdminWindowController parent) {
+        this.parent = parent;
+    }
+
     @FXML
-    void onAddPressed() {
+    void onAddPressed() throws IOException {
         System.out.println("Add Pressed\n");
+
+        this.parent.switchTo(SIDEBAR_ADD);
     }
 
     @FXML
--- KioskApplication/src/KioskApplication/controller/AdminWindowController.java
+++ KioskApplication/src/KioskApplication/controller/AdminWindowController.java
@@ -5,22 +5,50 @@ import javafx.fxml.FXMLLoader;
 
 import java.io.IOException;
 
+import static KioskApplication.controller.AdminWindowController.SidebarType.SIDEBAR_MENU;
+
 @DefaultProperty(value = "adminWindow")
 public class AdminWindowController extends MapWindowController {
+
+    public enum SidebarType {
+        SIDEBAR_ADD,
+        SIDEBAR_EDIT,
+        SIDEBAR_MENU
+    }
 
     public AdminWindowController() throws IOException {
         super();
 
-        FXMLLoader loader = new FXMLLoader(MapWindowController.class.getResource("/KioskApplication/view/AdminSidebarView.fxml"));
-        loader.setRoot(getSidebarPane());
-        loader.setController(new AdminSidebarController());
-        loader.load();
+        switchTo(SIDEBAR_MENU);
 
         getMapController().setParent(this);
     }
 
     void mapLocationClicked(double x, double y) {
         System.out.println(String.format("Admin Map Clicked: %f %f\n", x, y));
+    }
+
+    public void switchTo(SidebarType sidebar) throws IOException {
+        FXMLLoader loader = null;
+
+        switch (sidebar) {
+            case SIDEBAR_MENU:
+                loader = new FXMLLoader(getClass().getResource("/KioskApplication/view/AdminSidebarView.fxml"));
+                loader.setController(new AdminSidebarController(this));
+                break;
+            case SIDEBAR_ADD:
+                loader = new FXMLLoader(getClass().getResource("/KioskApplication/view/addNode.fxml"));
+                loader.setController(new AdminAddNodeController(this));
+                break;
+
+            case SIDEBAR_EDIT:
+                break;
+        }
+
+        if(loader != null) {
+            getSidebarPane().getChildren().clear();
+            getSidebarPane().getChildren().add(loader.load());
+        }
     }
 
     void mapNodeClicked() {}
