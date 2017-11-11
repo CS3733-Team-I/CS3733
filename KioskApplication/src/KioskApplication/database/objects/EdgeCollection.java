f6dc3a36e0c64a68fef991201d38545ff053298e
Ben Gillette <bkgillette@wpi.edu>
2017-11-11 15:29:26 -0500

    Copied over database code.

---
+++ KioskApplication/src/KioskApplication/database/objects/EdgeCollection.java
@@ -0,0 +1,31 @@
+package database.objects;
+
+import java.util.HashMap;
+
+public class EdgeCollection {
+    private static EdgeCollection instance = null;
+    private HashMap<String, Edge> edgeHashMap = new HashMap<>();
+
+    protected EdgeCollection() {
+
+    }
+
+    public void addEdge(Edge edge) {
+        edgeHashMap.put(edge.getEdgeID(), edge);
+    }
+
+    public Edge getEdge(String id) {
+        return edgeHashMap.get(id);
+    }
+
+    public void deleteEdge(String id) {
+        edgeHashMap.remove(id);
+    }
+
+    public static EdgeCollection getInstance() {
+        if(instance == null) {
+            instance = new EdgeCollection();
+        }
+        return instance;
+    }
+}
---
+++ KioskApplication/src/KioskApplication/database/objects/Node.java
@@ -0,0 +1,93 @@
+package KioskApplication.database.objects;
+
+public class Node {
+    private String nodeID;
+    private int xcoord;
+    private int ycoord;
+    private String floor;
+    private String building;
+    private String nodeType;
+    private String longName;
+    private String shortName;
+    private String teamAssigned;
+
+    public Node(String nodeID, int xcoord, int ycoord, String floor, String building, String nodeType, String longName, String shortName, String teamAssigned) {
+        this.nodeID = nodeID;
+        this.xcoord = xcoord;
+        this.ycoord = ycoord;
+        this.floor = floor;
+        this.building = building;
+        this.nodeType = nodeType;
+        this.longName = longName;
+        this.shortName = shortName;
+        this.teamAssigned = teamAssigned;
+    }
+
+    public int getXcoord() {
+        return xcoord;
+    }
+
+    public void setXcoord(int xcoord) {
+        this.xcoord = xcoord;
+    }
+
+    public int getYcoord() {
+        return ycoord;
+    }
+
+    public void setYcoord(int ycoord) {
+        this.ycoord = ycoord;
+    }
+
+    public String getFloor() {
+        return floor;
+    }
+
+    public void setFloor(String floor) {
+        this.floor = floor;
+    }
+
+    public String getBuilding() {
+        return building;
+    }
+
+    public void setBuilding(String building) {
+        this.building = building;
+    }
+
+    public String getNodeType() {
+        return nodeType;
+    }
+
+    public void setNodeType(String nodeType) {
+        this.nodeType = nodeType;
+    }
+
+    public String getLongName() {
+        return longName;
+    }
+
+    public void setLongName(String longName) {
+        this.longName = longName;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
+    public String getTeamAssigned() {
+        return teamAssigned;
+    }
+
+    public void setTeamAssigned(String teamAssigned) {
+        this.teamAssigned = teamAssigned;
+    }
+
+    public String getNodeID() {
+        return nodeID;
+    }
+}
