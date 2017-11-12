059159a5fe46687e9f0419c316cb15dc7222eae9
Michael <m.sidler456@gmail.com>
2017-11-12 12:52:16 -0500

    Editing tests

--- KioskApplication/src/KioskApplication/tests/entityTests/TestMapEntity.java
+++ KioskApplication/src/KioskApplication/tests/entityTests/TestMapEntity.java
@@ -1,5 +1,6 @@
 package KioskApplication.tests.entityTests;
 
+import KioskApplication.database.objects.Edge;
 import KioskApplication.database.objects.Node;
 import KioskApplication.entity.MapEntity;
 import KioskApplication.entity.MapFloorEntity;
@@ -12,15 +13,17 @@ public class TestMapEntity {
 
     MapEntity m;
     Node n1;
+    Edge e1;
 
     @Before
     public void setup() {
         m = new MapEntity();
         n1 = new Node("ID");
+        e1 = new Edge();
     }
 
     @Test
-    public void testAll() {
-        //TODO
+    public void testEdges() {
+        m.addEdge();
     }
 }
