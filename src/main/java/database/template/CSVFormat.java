49b3fb77737c6a47f574eb4962e4a82d92a32d64
John F Parrick <jfparrick@wpi.edu>
2017-11-12 13:45:10 -0500

    Delete CSVFormat.java
    Duplicate code

--- src/main/java/database/template/CSVFormat.java
+++
@@ -1,6 +0,0 @@
-package database.template;
-
-public class CSVFormat {
-    public static final String NODE_CSV_HEAD = "nodeID,xcoord,ycoord,floor,building,nodeType,longName,shortName,teamAssigned";
-    public static final String EDGE_CSV_HEAD = "nodeID,xcoord,ycoord,floor,building,nodeType,longName,shortName,teamAssigned";
-}
