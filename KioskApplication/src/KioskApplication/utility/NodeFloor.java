e9fec827c815c98e2a15a6c19baad3e6886bb2d4
Jerry Brown <jbbrown@wpi.edu>
2017-11-12 21:22:18 -0500

    Switched nodes to use enums.

---
+++ KioskApplication/src/KioskApplication/utility/NodeFloor.java
@@ -0,0 +1,10 @@
+package KioskApplication.database.util;
+
+public enum NodeFloor {
+    GROUND,
+    LOWERLEVEL1,
+    LOWERLEVEL_2,
+    FIRST,
+    SECOND,
+    THIRD
+}
---
+++ KioskApplication/src/KioskApplication/utility/NodeType.java
@@ -0,0 +1,4 @@
+package KioskApplication.utility;
+
+public enum NodeType {
+}
