52128a5c353cce896f18963bf3a9b9fc6289ed5e
hdunphy <hdunphy15@gmail.com>
2017-11-13 18:18:06 -0500

    Changed Request Button to Request dropdown Menu

--- KioskApplication/src/KioskApplication/controller/InterpreterRequestController.java
+++ KioskApplication/src/KioskApplication/controller/InterpreterRequestController.java
@@ -86,8 +86,8 @@ public class InterpreterRequestController {
         }
 
         //get from login information
-        LoginController LC = new LoginController();
-        String adminEmail = LoginController
+        //LoginController LC = new LoginController();
+        //String adminEmail = LoginController
 
         /*creates the request object
         InterpreterRequest newRequest = new InterpreterRequest(location, adminEmail, language);
