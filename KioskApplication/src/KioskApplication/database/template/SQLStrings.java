30f23861ffc89eba3be793714d62d5b655e9a85f
John Parrick <jfparrick@wpi.edu>
2017-11-13 20:01:25 -0500

    fixed the select node issue

--- KioskApplication/src/KioskApplication/database/template/SQLStrings.java
+++ KioskApplication/src/KioskApplication/database/template/SQLStrings.java
@@ -43,26 +43,26 @@ public class SQLStrings {
 
     public static final String EDGE_INSERT = "insert into t_edges values(?, ?, ?)";
     public static final String EDGE_UPDATE = "update t_edges set startNode=?, endNode=? where edgeID=?";
-    public static final String EDGE_SELECT = "SELECT ? FROM t_edges";
+    public static final String EDGE_SELECT = "SELECT * FROM t_edges where edgeID=?";
     public static final String EDGE_DELETE = "DELETE FROM t_edges where edgeID = ?";
     public static final String EDGE_SELECT_ALL = "select * from t_edges";
 
     public static final String NODE_INSERT = "insert into t_nodes values(?, ?, ?, ?, ?, ?, ?, ?, ?)";
     public static final String NODE_UPDATE = "update t_nodes set xcoord=?, ycoord=?, floor=?, building=?, nodeType=?, longName=?, shortName=?, teamAssigned=? where edgeID=?";
-    public static final String NODE_SELECT = "SELECT ? FROM t_nodes";
+    public static final String NODE_SELECT = "SELECT * FROM t_nodes where nodeID=?";
     public static final String NODE_DELETE = "DELETE FROM t_nodes WHERE nodesID = ?";
     public static final String NODE_SELECT_ALL = "SELECT * FROM T_NODES";
 
 
     public static final String REQUEST_INSERT = "insert into t_requests values(?, ?, ?)";
     public static final String REQUEST_UPDATE = "update t_requests set locationNode=?, set employee=? where requestID=?";
-    public static final String REQUEST_SELECT = "select ? from t_requests";
+    public static final String REQUEST_SELECT = "select * from t_requests where requestID=?";
     public static final String REQUEST_DELETE = "delete from t_requests where requestID=?";
     public static final String REQUEST_SELECT_ALL = "select * from t_requests";
 
     public static final String INTERPRETER_INSERT = "insert into t_interpreters values(?, ?, ?)";
     public static final String INTERPRETER_UPDATE = "update t_interpreters set language=?, requestID=? where interpreterID=?";
-    public static final String INTERPRETER_SELECT = "select ? from t_interpreters";
+    public static final String INTERPRETER_SELECT = "select * from t_interpreters where interpreterID=?";
     public static final String INTERPRETER_DELETE = "delete from t_interpreters where interpreterID=?";
     public static final String INTERPRETER_SELECT_ALL = "select * from t_interpreters";
 }
