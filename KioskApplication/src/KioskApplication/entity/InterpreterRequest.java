d4093d4fdaa0f1026783b4399a9e136f80033140
hdunphy <hdunphy15@gmail.com>
2017-11-13 19:46:24 -0500

    Fixed database connections error due to Requests and InterpreterRequests not being updated when pulling Dev

--- KioskApplication/src/KioskApplication/entity/InterpreterRequest.java
+++ KioskApplication/src/KioskApplication/entity/InterpreterRequest.java
@@ -1,13 +1,31 @@
 package KioskApplication.entity;
 
+import KioskApplication.database.DatabaseController;
 import KioskApplication.database.objects.Node;
 
 //Adds the field language
-public class InterpreterRequest extends Request{
+public class InterpreterRequest{
     String language;
+    int interpreterID;
+    Request request;
 
-    public InterpreterRequest(String location, String employee, String language) {
-        super(location, employee);
+    public InterpreterRequest(Node location, String employee, String language,
+                              int requestID, int interpreterID) {
+        request = DatabaseController.addRequest(requestID, location.getNodeID(), employee);
         this.language = language;
+        this.interpreterID = interpreterID;
+    }
+
+    public InterpreterRequest(String locationID, String employee, String language,
+                              int requestID, int interpreterID) {
+        request = DatabaseController.addRequest(requestID, locationID, employee);
+        this.language = language;
+        this.interpreterID = interpreterID;
+    }
+
+    public InterpreterRequest(String language, int interpreterID, int requestID) {
+        request = DatabaseController.getRequest(requestID);
+        this.language = language;
+        this.interpreterID = interpreterID;
     }
 }
--- KioskApplication/src/KioskApplication/entity/Request.java
+++ KioskApplication/src/KioskApplication/entity/Request.java
@@ -1,15 +1,24 @@
 package KioskApplication.entity;
 
+import KioskApplication.database.DatabaseController;
 import KioskApplication.database.objects.Node;
 
 //Class object that creates a 'request' which stores the information of a request
 //This object can then be passed to the database as well as to the active requests list
 public class Request {
-    String location;
+    Node location;
     String employee;
+    int requestID;
 
-    public Request(String location, String employee) {
+    public Request(Node location, String employee, int requestID) {
         this.location = location;
         this.employee = employee;
+        this.requestID = requestID;
+    }
+
+    public Request(String nodeID, String employee, int requestID) {
+        this.location = DatabaseController.getNode(nodeID);
+        this.employee = employee;
+        this.requestID = requestID;
     }
 }
