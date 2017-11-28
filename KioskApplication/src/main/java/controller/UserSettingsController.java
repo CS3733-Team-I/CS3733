770fa411dc7cfc9127f78d374aee0c07a76814c0
Jerry Brown <jbbrown@wpi.edu>
2017-11-28 00:06:56 -0500

    Added users to table.

--- KioskApplication/src/main/java/controller/UserSettingsController.java
+++ KioskApplication/src/main/java/controller/UserSettingsController.java
@@ -4,9 +4,17 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
 import com.jfoenix.controls.JFXTreeTableView;
+import database.objects.Employee;
+import entity.LoginEntity;
+import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.layout.GridPane;
+
+import java.util.ArrayList;
 
 public class UserSettingsController {
 
@@ -22,7 +30,47 @@ public class UserSettingsController {
     @FXML private JFXButton userActionButton;
     @FXML private Label userDialogLabel;
 
-    @FXML private JFXTreeTableView<?> usersList;
+    @FXML private JFXTreeTableView<Employee> usersList;
+    @FXML private GridPane userEditorPane;
+
+    @FXML
+    void initialize() {
+        userEditorPane.setVisible(false);
+
+        ArrayList<Employee> logins = LoginEntity.getInstance().getAllLogins();
+
+        final TreeItem<Employee> root = new TreeItem<>();
+        root.setExpanded(true);
+
+        logins.stream().forEach((employee) -> {
+            root.getChildren().add(new TreeItem<>(employee));
+        });
+
+        TreeTableColumn<Employee, String> usernameColumn = new TreeTableColumn<>("Username");
+        usernameColumn.setPrefWidth(150);
+        usernameColumn.setCellValueFactory(
+                (TreeTableColumn.CellDataFeatures<Employee, String> param) ->
+                        new ReadOnlyStringWrapper(param.getValue().getValue().getUserName())
+        );
+
+        TreeTableColumn<Employee, String> permissionColumn = new TreeTableColumn<>("Permission");
+        permissionColumn.setPrefWidth(150);
+        permissionColumn.setCellValueFactory(
+                (TreeTableColumn.CellDataFeatures<Employee, String> param) ->
+                        new ReadOnlyStringWrapper(param.getValue().getValue().getPermission().toString())
+        );
+
+        TreeTableColumn<Employee, String> serviceColumn = new TreeTableColumn<>("Service Availability");
+        serviceColumn.setPrefWidth(190);
+        serviceColumn.setCellValueFactory(
+                (TreeTableColumn.CellDataFeatures<Employee, String> param) ->
+                        new ReadOnlyStringWrapper(param.getValue().getValue().getServiceAbility().toString())
+        );
+
+        usersList.getColumns().setAll(usernameColumn, permissionColumn, serviceColumn);
+        usersList.setRoot(root);
+        usersList.setShowRoot(false);
+    }
 
     @FXML
     void onAddPressed(ActionEvent event) {
