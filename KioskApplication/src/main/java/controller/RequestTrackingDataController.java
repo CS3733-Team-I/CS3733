0cbfdaae164b1b52df9d792055bd3bf769d86d67
JRTaylord <james.taylor2@comcast.net>
2017-11-28 14:47:31 -0500

    Got the request tracking data screen to open and close

--- KioskApplication/src/main/java/controller/RequestTrackingDataController.java
+++ KioskApplication/src/main/java/controller/RequestTrackingDataController.java
@@ -1,24 +1,40 @@
 package controller;
 
+import database.objects.Edge;
 import entity.RequestEntity;
 import javafx.fxml.FXML;
+import javafx.geometry.Point2D;
+import javafx.scene.Node;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.XYChart;
+import utility.Node.NodeFloor;
 import utility.Request.LanguageFrequency;
 
 import java.util.LinkedList;
 
 public class RequestTrackingDataController {
-    @FXML
-    BarChart LanguageHistogram;
+    @FXML BarChart languageHistogramChart;
     RequestEntity r;
+    MainWindowController parent;
 
-    public RequestTrackingDataController(ScreenController parent, LinkedList<LanguageFrequency> lHData){
+    public RequestTrackingDataController(MainWindowController parent){
+        this.parent=parent;
         r=RequestEntity.getInstance();
-        XYChart.Series s = new XYChart.Series();
-        for (LanguageFrequency lF: lHData
-             ) {
+    }
+
+    @FXML
+    public void initialize(){
+
+    }
+
+    public void refreshTable(){
+    XYChart.Series s = new XYChart.Series();
+        for (LanguageFrequency lF: r.getLanguageFrequency()) {
             s.getData().add(new XYChart.Data(lF.getLanguage().toString(), lF.getFrequency()));
         }
+    }
+
+    public void closePanel(){
+        parent.closeRequestTrackingTable();
     }
 }
