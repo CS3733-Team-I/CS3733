ef92358639665ff035f283567c89dee80c73e0c2
JRTaylord <james.taylor2@comcast.net>
2017-11-29 00:07:51 -0500

    darkened fonts

--- KioskApplication/src/main/java/controller/RequestSubmitterController.java
+++ KioskApplication/src/main/java/controller/RequestSubmitterController.java
@@ -1,16 +1,12 @@
 package controller;
 
 import com.jfoenix.controls.*;
-import database.DatabaseController;
 import database.objects.Edge;
 import database.objects.Node;
-import database.objects.Request;
-import entity.MapEntity;
 import entity.LoginEntity;
 import entity.RequestEntity;
 import javafx.fxml.FXML;
 import javafx.geometry.Point2D;
-import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -20,11 +16,7 @@ import utility.request.Language;
 import utility.node.NodeFloor;
 import utility.request.RequestType;
 
-import java.awt.*;
-import java.awt.event.MouseEvent;
 import java.io.IOException;
-import java.util.LinkedList;
-import java.util.concurrent.TimeUnit;
 
 public class RequestSubmitterController extends ScreenController {
 
@@ -158,7 +150,7 @@ public class RequestSubmitterController extends ScreenController {
     @Override
     public javafx.scene.Node getContentView() {
         if (contentView == null) {
-            contentView = loadView("/view/RequestSubmitterView.fxml");
+            contentView = loadView("/view/RequestSubmitter.fxml");
         }
         return contentView;
     }
