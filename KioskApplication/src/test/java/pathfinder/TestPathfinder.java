28b3c8d352e94e475bb63f22e0498a3bf2e4ad96
Michael <m.sidler456@gmail.com>
2017-12-03 18:47:28 -0500

    Tests should work again

--- KioskApplication/src/test/java/pathfinder/TestPathfinder.java
+++ KioskApplication/src/test/java/pathfinder/TestPathfinder.java
@@ -26,16 +26,16 @@ public class TestPathfinder {
     private static Node n01, n02, n03, n04, n05, n06, n07, n08, n09, n10, n11, n12,
                         n13, n14, n15, n16, n17, n18, n19, n20, n21, n22, n23,
                         n24, n25, n26, n27, n28, n29, n30, n31, n32, n33, n34, n35,
-                        n36, n37, n38, n39, n40, n41, n42, n43, n44, n45, n46,
+                        n36, n37, n38, n39, n40,
                         n47, n48, n49, n50, n51, n52, n53, n54, n55, n56, n57, n58,
-                        n59, n60, n61, n62, n63, n64, n65, n66, n67, n68, n69;
+                        n59, n60, n61, n62, n63;
 
     private static Edge e01, e02, e03, e04, e05, e06, e07, e08, e09, e10, e11,
                         e12, e13, e14, e15, e16, e17, e18, e19, e20, e21, e22,
                         e23, e24, e25, e26, e27, e28, e29, e30, e31, e32, e33,
-                        e34, e35, e36, e37, e38, e39, e40, e41, e42, e43, e44,
+                        e34, e35, e36, e37, e38, e39, e40,
                         e45, e46, e47, e48, e49, e50, e51, e52, e53, e54, e55,
-                        e56, e57, e58, e59, e60, e61, e62, e63, e64, e65, e66,
+                        e56, e57, e58, e59, e60, e61, e62,
                         e67, e68, e69, e70, e71, e72, e73, e74, e75, e76;
     private static MapEntity map;
 
@@ -73,12 +73,6 @@ public class TestPathfinder {
                            |
        40                 n40
 
-       50                 n41   n42
-
-       60                 n43 - n44
-                           |     |
-       70                 n45 - n46
-
 
                         NodeFloor.FIRST
           X   10    20    30    40    50    60    70    80              n47 & n62 are elevators
@@ -91,11 +85,6 @@ public class TestPathfinder {
                            |
        40                 n63
 
-       50                 n64   n65
-
-       60                 n66 - n67
-                           |     |
-       70                 n68 - n69
     */
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -150,12 +139,6 @@ public class TestPathfinder {
         n38 = new Node("NODE38",50,30, NodeFloor.SECOND, NodeBuilding.FRANCIS45, NodeType.HALL, "NODE38_LN","NODE38_SN","I");
         n39 = new Node("NODE39",60,30, NodeFloor.SECOND, NodeBuilding.FRANCIS45, NodeType.ELEV, "Elevator B","Elev B","I");
         n40 = new Node("NODE40",30,40, NodeFloor.SECOND, NodeBuilding.FRANCIS45, NodeType.STAI, "Stair B","St B","I");
-        n41 = new Node("NODE41",30,50, NodeFloor.SECOND, NodeBuilding.FRANCIS45, NodeType.HALL, "NODE41_LN","NODE41_SN","I");
-        n42 = new Node("NODE42",40,50, NodeFloor.SECOND, NodeBuilding.FRANCIS45, NodeType.HALL, "NODE42_LN","NODE42_SN","I");
-        n43 = new Node("NODE43",30,60, NodeFloor.SECOND, NodeBuilding.FRANCIS45, NodeType.HALL, "NODE43_LN","NODE43_SN","I");
-        n44 = new Node("NODE44",40,60, NodeFloor.SECOND, NodeBuilding.FRANCIS45, NodeType.HALL, "NODE44_LN","NODE44_SN","I");
-        n45 = new Node("NODE45",30,70, NodeFloor.SECOND, NodeBuilding.FRANCIS45, NodeType.HALL, "NODE45_LN","NODE45_SN","I");
-        n46 = new Node("NODE46",40,70, NodeFloor.SECOND, NodeBuilding.FRANCIS45, NodeType.HALL, "NODE46_LN","NODE46_SN","I");
         //Nodes for F1
         n47 = new Node("NODE47",10,10, NodeFloor.FIRST, NodeBuilding.FRANCIS45, NodeType.ELEV, "Elevator A","Elev A","I");
         n48 = new Node("NODE48",20,10, NodeFloor.FIRST, NodeBuilding.FRANCIS45, NodeType.HALL, "NODE48_LN","NODE48_SN","I");
@@ -174,12 +157,6 @@ public class TestPathfinder {
         n61 = new Node("NODE61",50,30, NodeFloor.FIRST, NodeBuilding.FRANCIS45, NodeType.HALL, "NODE61_LN","NODE61_SN","I");
         n62 = new Node("NODE62",60,30, NodeFloor.FIRST, NodeBuilding.FRANCIS45, NodeType.ELEV, "Elevator B","Elev B","I");
         n63 = new Node("NODE63",30,40, NodeFloor.FIRST, NodeBuilding.FRANCIS45, NodeType.STAI, "Stair B","St B","I");
-        n64 = new Node("NODE64",30,50, NodeFloor.FIRST, NodeBuilding.FRANCIS45, NodeType.HALL, "NODE64_LN","NODE64_SN","I");
-        n65 = new Node("NODE65",40,50, NodeFloor.FIRST, NodeBuilding.FRANCIS45, NodeType.HALL, "NODE65_LN","NODE65_SN","I");
-        n66 = new Node("NODE66",30,60, NodeFloor.FIRST, NodeBuilding.FRANCIS45, NodeType.HALL, "NODE66_LN","NODE66_SN","I");
-        n67 = new Node("NODE67",40,60, NodeFloor.FIRST, NodeBuilding.FRANCIS45, NodeType.HALL, "NODE67_LN","NODE67_SN","I");
-        n68 = new Node("NODE68",30,70, NodeFloor.FIRST, NodeBuilding.FRANCIS45, NodeType.HALL, "NODE68_LN","NODE68_SN","I");
-        n69 = new Node("NODE69",40,70, NodeFloor.FIRST, NodeBuilding.FRANCIS45, NodeType.HALL, "NODE69_LN","NODE69_SN","I");
 
         //Add all nodes to the map
         map.addNode(n01); map.addNode(n02); map.addNode(n03); map.addNode(n04); map.addNode(n05);
@@ -190,13 +167,11 @@ public class TestPathfinder {
         map.addNode(n24); map.addNode(n25); map.addNode(n26); map.addNode(n27); map.addNode(n28);
         map.addNode(n29); map.addNode(n30); map.addNode(n31); map.addNode(n32); map.addNode(n33);
         map.addNode(n34); map.addNode(n35); map.addNode(n36); map.addNode(n37); map.addNode(n38);
-        map.addNode(n39); map.addNode(n40); map.addNode(n41); map.addNode(n42); map.addNode(n43);
-        map.addNode(n44); map.addNode(n45); map.addNode(n46);
+        map.addNode(n39); map.addNode(n40);
         map.addNode(n47); map.addNode(n48); map.addNode(n49); map.addNode(n50); map.addNode(n51);
         map.addNode(n52); map.addNode(n53); map.addNode(n54); map.addNode(n55); map.addNode(n56);
         map.addNode(n57); map.addNode(n58); map.addNode(n59); map.addNode(n60); map.addNode(n61);
-        map.addNode(n62); map.addNode(n63); map.addNode(n64); map.addNode(n65); map.addNode(n66);
-        map.addNode(n67); map.addNode(n68); map.addNode(n69);
+        map.addNode(n62); map.addNode(n63);
 
         //Edges for F3
         e01 = new Edge("EDGE01", n01.getNodeID(), n02.getNodeID());
@@ -208,65 +183,57 @@ public class TestPathfinder {
         e07 = new Edge("EDGE07", n02.getNodeID(), n08.getNodeID());
         e08 = new Edge("EDGE08", n04.getNodeID(), n10.getNodeID());
         e09 = new Edge("EDGE09", n08.getNodeID(), n09.getNodeID());
-        e10 = new Edge("EDGE10", n10.getNodeID(), n15.getNodeID());
-        e11 = new Edge("EDGE11", n08.getNodeID(), n11.getNodeID());
-        e12 = new Edge("EDGE12", n10.getNodeID(), n13.getNodeID());
-        e13 = new Edge("EDGE13", n15.getNodeID(), n14.getNodeID());
-        e14 = new Edge("EDGE14", n11.getNodeID(), n12.getNodeID());
-        e15 = new Edge("EDGE15", n12.getNodeID(), n13.getNodeID());
-        e16 = new Edge("EDGE16", n13.getNodeID(), n14.getNodeID());
-        e17 = new Edge("EDGE17", n14.getNodeID(), n16.getNodeID());
-        e18 = new Edge("EDGE18", n12.getNodeID(), n17.getNodeID());
+        e10 = new Edge("EDGE10", n10.getNodeID(), n11.getNodeID());
+        e11 = new Edge("EDGE11", n08.getNodeID(), n12.getNodeID());
+        e12 = new Edge("EDGE12", n10.getNodeID(), n14.getNodeID());
+        e13 = new Edge("EDGE13", n11.getNodeID(), n15.getNodeID());
+        e14 = new Edge("EDGE14", n12.getNodeID(), n13.getNodeID());
+        e15 = new Edge("EDGE15", n13.getNodeID(), n14.getNodeID());
+        e16 = new Edge("EDGE16", n14.getNodeID(), n15.getNodeID());
+        e17 = new Edge("EDGE17", n15.getNodeID(), n16.getNodeID());
+        e18 = new Edge("EDGE18", n13.getNodeID(), n17.getNodeID());
         e19 = new Edge("EDGE19", n20.getNodeID(), n21.getNodeID());
         e20 = new Edge("EDGE20", n20.getNodeID(), n22.getNodeID());
         e21 = new Edge("EDGE21", n21.getNodeID(), n23.getNodeID());
         e22 = new Edge("EDGE22", n22.getNodeID(), n23.getNodeID());
         //Edges for F2
-        e23 = new Edge("EDGE23", n01.getNodeID(), n02.getNodeID());
-        e24 = new Edge("EDGE24", n02.getNodeID(), n03.getNodeID());
-        e25 = new Edge("EDGE25", n03.getNodeID(), n04.getNodeID());
-        e26 = new Edge("EDGE26", n04.getNodeID(), n05.getNodeID());
-        e27 = new Edge("EDGE27", n05.getNodeID(), n06.getNodeID());
-        e28 = new Edge("EDGE28", n06.getNodeID(), n07.getNodeID());
-        e29 = new Edge("EDGE29", n02.getNodeID(), n08.getNodeID());
-        e30 = new Edge("EDGE30", n04.getNodeID(), n10.getNodeID());
-        e31 = new Edge("EDGE31", n08.getNodeID(), n09.getNodeID());
-        e32 = new Edge("EDGE32", n10.getNodeID(), n15.getNodeID());
-        e33 = new Edge("EDGE33", n08.getNodeID(), n11.getNodeID());
-        e34 = new Edge("EDGE34", n10.getNodeID(), n13.getNodeID());
-        e35 = new Edge("EDGE35", n15.getNodeID(), n14.getNodeID());
-        e36 = new Edge("EDGE36", n11.getNodeID(), n12.getNodeID());
-        e37 = new Edge("EDGE37", n12.getNodeID(), n13.getNodeID());
-        e38 = new Edge("EDGE38", n13.getNodeID(), n14.getNodeID());
-        e39 = new Edge("EDGE39", n14.getNodeID(), n16.getNodeID());
-        e40 = new Edge("EDGE40", n12.getNodeID(), n17.getNodeID());
-        e41 = new Edge("EDGE41", n20.getNodeID(), n21.getNodeID());
-        e42 = new Edge("EDGE42", n20.getNodeID(), n22.getNodeID());
-        e43 = new Edge("EDGE43", n21.getNodeID(), n23.getNodeID());
-        e44 = new Edge("EDGE44", n22.getNodeID(), n23.getNodeID());
+        e23 = new Edge("EDGE23", n24.getNodeID(), n25.getNodeID());
+        e24 = new Edge("EDGE24", n25.getNodeID(), n26.getNodeID());
+        e25 = new Edge("EDGE25", n26.getNodeID(), n27.getNodeID());
+        e26 = new Edge("EDGE26", n27.getNodeID(), n28.getNodeID());
+        e27 = new Edge("EDGE27", n28.getNodeID(), n29.getNodeID());
+        e28 = new Edge("EDGE28", n29.getNodeID(), n30.getNodeID());
+        e29 = new Edge("EDGE29", n25.getNodeID(), n31.getNodeID());
+        e30 = new Edge("EDGE30", n27.getNodeID(), n33.getNodeID());
+        e31 = new Edge("EDGE31", n31.getNodeID(), n32.getNodeID());
+        e32 = new Edge("EDGE32", n33.getNodeID(), n34.getNodeID());
+        e33 = new Edge("EDGE33", n31.getNodeID(), n35.getNodeID());
+        e34 = new Edge("EDGE34", n33.getNodeID(), n37.getNodeID());
+        e35 = new Edge("EDGE35", n34.getNodeID(), n38.getNodeID());
+        e36 = new Edge("EDGE36", n35.getNodeID(), n36.getNodeID());
+        e37 = new Edge("EDGE37", n36.getNodeID(), n37.getNodeID());
+        e38 = new Edge("EDGE38", n37.getNodeID(), n38.getNodeID());
+        e39 = new Edge("EDGE39", n38.getNodeID(), n39.getNodeID());
+        e40 = new Edge("EDGE40", n36.getNodeID(), n40.getNodeID());
         //Edges for F1
-        e45 = new Edge("EDGE45", n01.getNodeID(), n02.getNodeID());
-        e46 = new Edge("EDGE46", n02.getNodeID(), n03.getNodeID());
-        e47 = new Edge("EDGE47", n03.getNodeID(), n04.getNodeID());
-        e48 = new Edge("EDGE48", n04.getNodeID(), n05.getNodeID());
-        e49 = new Edge("EDGE49", n05.getNodeID(), n06.getNodeID());
-        e50 = new Edge("EDGE50", n06.getNodeID(), n07.getNodeID());
-        e51 = new Edge("EDGE51", n02.getNodeID(), n08.getNodeID());
-        e52 = new Edge("EDGE52", n04.getNodeID(), n10.getNodeID());
-        e53 = new Edge("EDGE53", n08.getNodeID(), n09.getNodeID());
-        e54 = new Edge("EDGE54", n10.getNodeID(), n15.getNodeID());
-        e55 = new Edge("EDGE55", n08.getNodeID(), n11.getNodeID());
-        e56 = new Edge("EDGE56", n10.getNodeID(), n13.getNodeID());
-        e57 = new Edge("EDGE57", n15.getNodeID(), n14.getNodeID());
-        e58 = new Edge("EDGE58", n11.getNodeID(), n12.getNodeID());
-        e59 = new Edge("EDGE59", n12.getNodeID(), n13.getNodeID());
-        e60 = new Edge("EDGE60", n13.getNodeID(), n14.getNodeID());
-        e61 = new Edge("EDGE61", n14.getNodeID(), n16.getNodeID());
-        e62 = new Edge("EDGE62", n12.getNodeID(), n17.getNodeID());
-        e63 = new Edge("EDGE63", n20.getNodeID(), n21.getNodeID());
-        e64 = new Edge("EDGE64", n20.getNodeID(), n22.getNodeID());
-        e65 = new Edge("EDGE65", n21.getNodeID(), n23.getNodeID());
-        e66 = new Edge("EDGE66", n22.getNodeID(), n23.getNodeID());
+        e45 = new Edge("EDGE45", n47.getNodeID(), n48.getNodeID());
+        e46 = new Edge("EDGE46", n48.getNodeID(), n49.getNodeID());
+        e47 = new Edge("EDGE47", n49.getNodeID(), n50.getNodeID());
+        e48 = new Edge("EDGE48", n50.getNodeID(), n51.getNodeID());
+        e49 = new Edge("EDGE49", n51.getNodeID(), n52.getNodeID());
+        e50 = new Edge("EDGE50", n52.getNodeID(), n53.getNodeID());
+        e51 = new Edge("EDGE51", n48.getNodeID(), n54.getNodeID());
+        e52 = new Edge("EDGE52", n50.getNodeID(), n56.getNodeID());
+        e53 = new Edge("EDGE53", n54.getNodeID(), n55.getNodeID());
+        e54 = new Edge("EDGE54", n56.getNodeID(), n57.getNodeID());
+        e55 = new Edge("EDGE55", n54.getNodeID(), n58.getNodeID());
+        e56 = new Edge("EDGE56", n56.getNodeID(), n60.getNodeID());
+        e57 = new Edge("EDGE57", n57.getNodeID(), n61.getNodeID());
+        e58 = new Edge("EDGE58", n58.getNodeID(), n59.getNodeID());
+        e59 = new Edge("EDGE59", n59.getNodeID(), n60.getNodeID());
+        e60 = new Edge("EDGE60", n60.getNodeID(), n61.getNodeID());
+        e61 = new Edge("EDGE61", n61.getNodeID(), n62.getNodeID());
+        e62 = new Edge("EDGE62", n59.getNodeID(), n63.getNodeID());
         //Edges for elevators
         e67 = new Edge("EDGE67", n01.getNodeID(), n24.getNodeID());
         e68 = new Edge("EDGE68", n01.getNodeID(), n47.getNodeID());
@@ -289,13 +256,11 @@ public class TestPathfinder {
         map.addEdge(e23); map.addEdge(e24); map.addEdge(e25); map.addEdge(e26); map.addEdge(e27);
         map.addEdge(e28); map.addEdge(e29); map.addEdge(e30); map.addEdge(e31); map.addEdge(e32);
         map.addEdge(e33); map.addEdge(e34); map.addEdge(e35); map.addEdge(e36); map.addEdge(e37);
-        map.addEdge(e38); map.addEdge(e39); map.addEdge(e40); map.addEdge(e41); map.addEdge(e42);
-        map.addEdge(e43); map.addEdge(e44);
+        map.addEdge(e38); map.addEdge(e39); map.addEdge(e40);
         map.addEdge(e45); map.addEdge(e46); map.addEdge(e47); map.addEdge(e48); map.addEdge(e49);
         map.addEdge(e50); map.addEdge(e51); map.addEdge(e52); map.addEdge(e53); map.addEdge(e54);
         map.addEdge(e55); map.addEdge(e56); map.addEdge(e57); map.addEdge(e58); map.addEdge(e59);
-        map.addEdge(e60); map.addEdge(e61); map.addEdge(e62); map.addEdge(e63); map.addEdge(e64);
-        map.addEdge(e65); map.addEdge(e66);
+        map.addEdge(e60); map.addEdge(e61); map.addEdge(e62);
         map.addEdge(e67); map.addEdge(e68); map.addEdge(e69); map.addEdge(e70); map.addEdge(e71);
         map.addEdge(e72); map.addEdge(e73); map.addEdge(e74); map.addEdge(e75); map.addEdge(e76);
     }
