58c08d09c25a9ce3e82b9b09064741cf772c6266
Michael <m.sidler456@gmail.com>
2017-12-03 19:09:15 -0500

    Add javadoc comments

--- KioskApplication/src/main/java/database/objects/Edge.java
+++ KioskApplication/src/main/java/database/objects/Edge.java
@@ -13,6 +13,11 @@ public class Edge {
         this.node2ID = node2;
     }
 
+    /**
+     * Override of .equals()
+     * @param obj other edge object
+     * @return true if nodes are the same; otherwise false
+     */
     @Override
     public boolean equals(Object obj) {
         if(obj == null) return false;
@@ -26,26 +31,51 @@ public class Edge {
         }
     }
 
+    /**
+     * Gets the ID of this edge
+     * @return String of ID
+     */
     public String getEdgeID() {
         return edgeID;
     }
 
+    /**
+     * Gets the ID of node1
+     * @return String of ID
+     */
     public String getNode1ID() {
         return node1ID;
     }
 
+    /**
+     * Gets the ID of node2
+     * @return String of ID
+     */
     public String getNode2ID() {
         return node2ID;
     }
 
+    /**
+     * Sets the ID for node1
+     * @param node1 String of node ID
+     */
     public void setNode1ID(String node1) {
         this.node1ID = node1;
     }
 
+    /**
+     * Sets the ID for node2
+     * @param node2 String of node ID
+     */
     public void setNode2ID(String node2) {
         this.node2ID = node2;
     }
 
+    /**
+     * Returns true if two the given edge is connected by having a common node
+     * @param e Another edge to compare nodes with
+     * @return True if there is a common node; otherwise false
+     */
     public boolean isConnectedTo(Edge e) {
         return this.node1ID.equals(e.getNode1ID())  ||
                 this.node1ID.equals(e.getNode2ID()) ||
@@ -53,6 +83,11 @@ public class Edge {
                 this.node2ID.equals(e.getNode2ID());
     }
 
+    /**
+     * Given one node ID return the other node ID or null if the given node isn't a node in the edge
+     * @param id ID of the given node
+     * @return the ID of the other connected node or null
+     */
     public String getOtherNodeID(String id) {
 
         if(this.node1ID.equals(id))
