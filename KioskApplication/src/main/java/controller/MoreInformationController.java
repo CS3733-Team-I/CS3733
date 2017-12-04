e48fb142d358a8f74b0f58f7f34d0694920810b5
hdunphy <hdunphy15@gmail.com>
2017-12-04 00:01:49 -0500

    Added information to the moreInformation controller

--- KioskApplication/src/main/java/controller/MoreInformationController.java
+++ KioskApplication/src/main/java/controller/MoreInformationController.java
@@ -4,11 +4,16 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextArea;
 import controller.map.MapController;
 import database.objects.Edge;
+import database.objects.Request;
+import entity.LoginEntity;
+import entity.MapEntity;
+import entity.RequestEntity;
 import javafx.fxml.FXML;
 import javafx.geometry.Point2D;
-import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
 import utility.node.NodeFloor;
+import utility.request.RequestProgressStatus;
+import utility.request.RequestType;
 
 public class MoreInformationController extends ScreenController {
 
@@ -16,15 +21,45 @@ public class MoreInformationController extends ScreenController {
     @FXML private JFXTextArea displayNotes;
 
     String requestID;
+    RequestEntity r;
+    LoginEntity l;
 
     public MoreInformationController(MainWindowController parent, MapController mapController, String requestID) {
         super(parent, mapController);
+        r = RequestEntity.getInstance();
+        l = LoginEntity.getInstance();
         this.requestID = requestID;
     }
 
     @FXML
     public void initialize(){
 
+
+
+        Request request = r.getRequest(requestID);
+        button1.setText(requestID);
+//        Label id = new Label(requestID);
+        RequestType RT = r.checkRequestType(requestID);
+        switch (RT){
+            case INTERPRETER:
+                String language = r.getInterpreterRequest(requestID).getLanguage().toString();
+                button2.setText("Language: "+language);
+                break;
+            default: //security
+                int priority = r.getSecurityRequest(requestID).getPriority();
+                button2.setText("Priority: "+ priority);
+                break;
+        }
+        String location = MapEntity.getInstance().getNode(request.getNodeID()).getLongName();
+        button3.setText(location);
+        if(request.getStatus().equals(RequestProgressStatus.TO_DO)){
+            button4.setText("Assigned by: "+request.getAssigner());
+            button5.setText("Sent in at: "+request.getSubmittedTime().toString());
+        }else{
+            button4.setText("Completed by: "+request.getCompleter());
+            button5.setText("Completed at"+request.getCompletedTime().toString());
+        }
+        displayNotes.setText(request.getNote());
     }
 
     @Override
