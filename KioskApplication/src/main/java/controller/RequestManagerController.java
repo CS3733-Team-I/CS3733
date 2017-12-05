be55dc7dc224f0adfb32c0a86a2677d9aaee2264
hdunphy <hdunphy15@gmail.com>
2017-12-04 20:42:14 -0500

    Last commit was to fix conflicts with login changes

--- KioskApplication/src/main/java/controller/RequestManagerController.java
+++ KioskApplication/src/main/java/controller/RequestManagerController.java
@@ -44,7 +44,6 @@ public class RequestManagerController extends ScreenController {
     @FXML private JFXPopup popup;
     @FXML private HBox row8;
     @FXML private HBox row9;
-
     //filter buttons
     @FXML private JFXCheckBox foodFilter;
     @FXML private JFXCheckBox janitorFilter;
