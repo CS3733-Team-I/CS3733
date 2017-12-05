fa1eb8284105b1062eb5c66a174fbd27c52f368a
JRTaylord <james.taylor2@comcast.net>
2017-12-05 14:33:26 -0500

    The resourceBundle getter in MainWindowController now works

--- KioskApplication/src/main/java/controller/MainWindowController.java
+++ KioskApplication/src/main/java/controller/MainWindowController.java
@@ -60,13 +60,13 @@ public class MainWindowController {
         // Initialize MapView with MapController
         mapController = new MapController();
         mapController.setParent(this);
-        /*ResourceBundle rb= ResourceBundle.getBundle("Internationalization.properties");
+        ResourceBundle rb= ResourceBundle.getBundle("Internationalization");
         Enumeration <String> keys = rb.getKeys();
         while (keys.hasMoreElements()) {
             String key = keys.nextElement();
             String value = rb.getString(key);
             System.out.println(key + ": " + value);
-        }*/
+        }
         FXMLLoader mapPaneLoader = new FXMLLoader(getClass().getResource("/view/MapView.fxml"));
         mapPaneLoader.setRoot(mapView);
         mapPaneLoader.setController(mapController);
