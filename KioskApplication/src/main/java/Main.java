7cf91600a4d249932345ad852bf028184860e0a1
John Parrick <jfparrick@wpi.edu>
2017-12-07 19:59:45 -0500

    Main Reverted

--- KioskApplication/src/main/java/Main.java
+++ KioskApplication/src/main/java/Main.java
@@ -1,15 +1,18 @@
-import email.Email;
-import email.EmailSender;
-import email.Providers;
-import email.SMS;
+import email.*;
+import entity.MapEntity;
+import entity.SystemSettings;
 import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import utility.csv.CsvFileUtil;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        /*
         MapEntity.getInstance().readAllFromDatabase();
         if (MapEntity.getInstance().getAllNodes().size() == 0)
             CsvFileUtil.getInstance().readAllCSVs();
@@ -25,14 +28,14 @@ public class Main extends Application {
 
         primaryStage.setScene(mainScene);
         primaryStage.show();
-        */
-        EmailSender.init();
+        /*
+        EmailSystem.init();
         /*Email email = new Email.Builder("jflparrick@gmail.com")
                 .setAttachment(null)
                 .setBody("THIS IS A TEST")
                 .setFrom("me")
                 .setSubject("TEST")
-                .build(); */
+                .build();
         SMS sms = new SMS.Builder("2077455316", Providers.VERIZON)
                 .setAttachment(null)
                 .setBody("THIS IS A TEST")
@@ -40,6 +43,7 @@ public class Main extends Application {
                 .setFrom("me")
                 .build();
         EmailSender.sendEmail(sms);
+        */
     }
 
     public static void main(String[] args) {
