ea54f0ce1c9ebf6d924fecd84f26040c67a35239
Mary Hatfalvi <mthatfalvi@wpi.edu>
2017-12-08 18:30:53 -0500

    fuzzy search algorithm written

--- KioskApplication/src/main/java/pathfinder/FuzzySearch.java
+++ KioskApplication/src/main/java/pathfinder/FuzzySearch.java
@@ -6,12 +6,20 @@ import entity.MapEntity;
 
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
+import java.lang.Object;
 import org.apache.commons.codec.language.DoubleMetaphone;
 
 public class FuzzySearch {
 
+   // private DoubleMetaphone metaphone;
     private DoubleMetaphone metaphone;
-
+    /**
+     * Fuzzy search
+     * using metaphone
+     * @param inputtext
+     * @return
+     */
     public Node FuzzySearch(String inputtext){
         // put all nodes in hash map
         //TODO probably better way of initalizing
@@ -23,20 +31,37 @@ public class FuzzySearch {
         for(Node node :mapAllNodes){
             allNodes.put(node.getNodeID(),node);
         }
-
+        metaphone = new DoubleMetaphone();
+        //Node bestmatch = allNodes.get(allNodes.keySet().iterator().next());
         String[] input= inputtext.split("");
 
-
-            for(String key: allNodes.keySet()){
-                    for (String text:input){
-                   // key.
+        if(input.length >0) {
+            Map<String,Integer> sortedMap = new HashMap<>();
+            int matched = 0;
+            for (String key : allNodes.keySet()) {
+                String[] longname = allNodes.get(key).getLongName().split("");
+                for (int i = 0; i < longname.length; i++) {
+                    matched = 0;
+                    for (String text : input) {
+                        if (metaphone.isDoubleMetaphoneEqual(text, longname[i])) {
+                            matched++;
+                        }
+                    }
+                    sortedMap.put(key, matched);
+                }
             }
+            int min = 0;
+            String match = allNodes.keySet().iterator().next();
+           for (String key: sortedMap.keySet()){
+                if(sortedMap.get(key)>min){
+                    match=key;
+                    min=sortedMap.get(key);
+                }
+            }
+
+            return allNodes.get(match);
         }
-
-        int min = 15;//initial value
-        int testVal=0;
-
-        //return matchedPerson;
-        return null;
+        else{
+        return null;}
     }
 }
