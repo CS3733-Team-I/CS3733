36716b10675eadfb8a6e7e15943980ec3ceeacbd
ApocalyVec <zli12@wpi.edu>
2017-12-10 19:54:35 -0500

    add request search type

--- KioskApplication/src/main/java/entity/searchEntity/SearchNode.java
+++ KioskApplication/src/main/java/entity/searchEntity/SearchNode.java
@@ -24,70 +24,50 @@ public class SearchNode implements ISearchEntity{
             case REST:
                 Image restroomIcon = ResourceManager.getInstance().getImage("/images/icons/restroom.png");
                 this.nodeIcon = new javafx.scene.image.ImageView(restroomIcon);
-                nodeIcon.setFitHeight(48);
-                nodeIcon.setFitWidth(48);
                 break;
             case ELEV:
                 Image elevIcon = ResourceManager.getInstance().getImage("/images/icons/elevator.png");
                 nodeIcon= new ImageView(elevIcon);
-                nodeIcon.setFitHeight(48);
-                nodeIcon.setFitWidth(48);
                 break;
             case EXIT:
                 Image exitIcon = ResourceManager.getInstance().getImage("/images/icons/exit.png");
                 nodeIcon = new ImageView(exitIcon);
-                nodeIcon.setFitHeight(48);
-                nodeIcon.setFitWidth(48);
                 break;
             case RETL:
                 Image foodIcon = ResourceManager.getInstance().getImage("/images/icons/food.png");
                 nodeIcon = new ImageView(foodIcon);
-                nodeIcon.setFitHeight(48);
-                nodeIcon.setFitWidth(48);
                 break;
             case CONF:
                 Image confIcon = ResourceManager.getInstance().getImage("/images/icons/conf.png");
                 nodeIcon = new ImageView(confIcon);
-                nodeIcon.setFitHeight(48);
-                nodeIcon.setFitWidth(48);
                 break;
             case DEPT:
                 Image deptIcon = ResourceManager.getInstance().getImage("/images/icons/department.png");
                 nodeIcon = new ImageView(deptIcon);
-                nodeIcon.setFitHeight(48);
-                nodeIcon.setFitWidth(48);
                 break;
             case LABS:
                 Image labIcon = ResourceManager.getInstance().getImage("/images/icons/lab.png");
                 nodeIcon = new ImageView(labIcon);
-                nodeIcon.setFitHeight(48);
-                nodeIcon.setFitWidth(48);
                 break;
             case INFO:
                 Image nodeInfoIcon = ResourceManager.getInstance().getImage("/images/icons/nodeInfo.png");
                 nodeIcon = new ImageView(nodeInfoIcon);
-                nodeIcon.setFitHeight(48);
-                nodeIcon.setFitWidth(48);
                 break;
             case SERV:
                 Image servIcon = ResourceManager.getInstance().getImage("/images/icons/service.png");
                 nodeIcon = new ImageView(servIcon);
-                nodeIcon.setFitHeight(48);
-                nodeIcon.setFitWidth(48);
                 break;
             case STAI:
                 Image staiIcon = ResourceManager.getInstance().getImage("/images/icons/stairs.png");
                 nodeIcon = new ImageView(staiIcon);
-                nodeIcon.setFitHeight(48);
-                nodeIcon.setFitWidth(48);
                 break;
             default:
                 //TODO change this
                 Image defaultIcon = ResourceManager.getInstance().getImage("/images/icons/nukeIcon.png");
                 nodeIcon = new ImageView(defaultIcon);
-                nodeIcon.setFitHeight(48);
-                nodeIcon.setFitWidth(48);
         }
+        nodeIcon.setFitHeight(48);
+        nodeIcon.setFitWidth(48);
     }
 
     public ImageView getIcon()
