78b1c15e15a66363ea1d44f91976d59c1d98d694
Michael <m.sidler456@gmail.com>
2017-12-11 19:44:01 -0500

    Fixed node creating error

--- KioskApplication/src/main/java/utility/node/NodeType.java
+++ KioskApplication/src/main/java/utility/node/NodeType.java
@@ -33,4 +33,8 @@ public enum NodeType {
             default: return "Not Set: Contact Programmer";
         }
     }
+
+    public String toLiteralString() {
+        return super.toString();
+    }
 }
